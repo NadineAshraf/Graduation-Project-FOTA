5882447ecdbaf06229a773f100c2bb559c8e5fe3
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-07-23 10:38:52 +0200

    Add files via upload

---
+++ WebSite/crud/models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from statistics import median
+
+from django.db import models
+from django.core.validators import RegexValidator
+
+
+# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=40, blank=False)
+    lastname = models.CharField(max_length=40, blank=False)
+    mobile_number = models.CharField(max_length=10, blank=True)
+    description = models.TextField(max_length=255, blank=False)
+    location = models.TextField(max_length=255, blank=False)
+    date = models.DateField('%m/%d/%Y')
+    created_at = models.DateTimeField('%m/%d/%Y %H:%M:%S')
+    updated_at = models.DateTimeField('%m/%d/%Y %H:%M:%S')
+
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
